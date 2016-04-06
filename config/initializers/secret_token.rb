65608bcbcb2415b827045becc0acc4a050640226
tony-jones <ad30jone@gmail.com>
2016-04-05 20:23:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PodcastFm::Application.config.secret_key_base = '23eaf84f1bf070faf2c3148f824f63df787157c3a426a60bf85f6b0fa7b8412e0483a8291230c1da00fe1ceb1ece3dd4f4aa351e7d3e0366e86cab090bc6f503'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PodcastFm::Application.config.session_store :cookie_store, key: '_podcast_fm_session'
