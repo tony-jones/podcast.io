8f90c136414db21e73e832e05bd9c2f8e72e7cbc
tony-jones <ad30jone@gmail.com>
2016-04-10 01:01:38 -0400

    added dashboard for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 PodcastFm::Application.routes.draw do
+
   devise_for :podcasts
-  root 'welcome#index'
 
   resources :podcasts, only: [:index, :show] do
     resources :episodes
   end
+
+  authenticated :podcast do
+    root 'podcasts#dashboard', as: "authenticated_root"
+  end
+
+  root 'welcome#index'
 end
