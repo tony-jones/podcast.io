9c41e7cea742d5c40d8e63b77c4fdea3c202971e
tony-jones <ad30jone@gmail.com>
2016-04-20 01:38:33 -0400

    pluralize episodes

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,5 +1,5 @@
 class Episode < ActiveRecord::Base
-  belongs_to :podcast
+  belongs_to :podcast, counter_cache: :count_of_orders
 
   has_attached_file :episode_thumbnail, :styles => { :large => "1000x1000#", :medium => "550x550#" }
   validates_attachment_content_type :episode_thumbnail, :content_type => /\Aimage\/.*\Z/
